fdecd8c044a307c9d8c45c9664f194d89780fc45
kadiace <rlacjfghks95@gmail.com>
2024-04-22 17:27:21 +0900

    Success to connect jest process.

--- src/modules/messages/messages.controller.spec.ts
+++ src/modules/messages/messages.controller.spec.ts
@@ -1,21 +1,45 @@
+import { INestApplication } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { TypeOrmConfigService } from '@configs/typeorm.config.service'
+
+import { Message } from '@entities/message.entity'
 
 import { MessagesController } from '@messages/messages.controller'
 import { MessagesService } from '@messages/messages.service'
 
 describe('MessagesController', () => {
   let controller: MessagesController
+  let app: INestApplication
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: TypeOrmConfigService,
+        }),
+        TypeOrmModule.forFeature([Message]),
+      ],
       controllers: [MessagesController],
       providers: [MessagesService],
     }).compile()
 
     controller = module.get<MessagesController>(MessagesController)
+    app = module.createNestApplication()
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  afterAll(async () => {
+    await app.close()
   })
 })
--- src/modules/messages/messages.service.spec.ts
+++ src/modules/messages/messages.service.spec.ts
@@ -1,19 +1,43 @@
+import { INestApplication } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { TypeOrmConfigService } from '@configs/typeorm.config.service'
+
+import { Message } from '@entities/message.entity'
 
 import { MessagesService } from '@messages/messages.service'
 
 describe('MessagesService', () => {
   let service: MessagesService
+  let app: INestApplication
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: TypeOrmConfigService,
+        }),
+        TypeOrmModule.forFeature([Message]),
+      ],
       providers: [MessagesService],
     }).compile()
 
     service = module.get<MessagesService>(MessagesService)
+    app = module.createNestApplication()
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  afterAll(async () => {
+    await app.close()
   })
 })
