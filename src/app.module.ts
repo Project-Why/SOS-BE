dd22c699522b5f8f959df606748eed1753eb712d
kadiace <rlacjfghks95@gmail.com>
2024-06-03 09:47:50 +0900

    Add throttler pipeline.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { TypeOrmConfigService } from '@configs/typeorm.config.service'
@@ -19,6 +20,18 @@ import { AppService } from './app.service'
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: '1m',
+        ttl: 60000,
+        limit: 3,
+      },
+      {
+        name: '10m',
+        ttl: 600000,
+        limit: 10,
+      },
+    ]),
     MessagesModule,
   ],
   controllers: [AppController],
