f6a545e1e5b241ee80458f6fd28624599dd9f359
kadiace <rlacjfghks95@gmail.com>
2024-07-03 12:22:42 +0900

    Add CORS option.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { INestApplication, Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
+import { isLocal } from '@utils'
+
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './middleware/exception-filter.middleware'
 
@@ -10,6 +12,13 @@ async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule)
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: isLocal()
+      ? 'http://localhost:3000'
+      : 'https://project-why.github.io',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   await app.listen(process.env.PORT)
 }
 
