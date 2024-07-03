cd1bb0ffd7af11ffb4823b141a1a6858d3beafcb
kadiace <rlacjfghks95@gmail.com>
2024-07-03 15:58:52 +0900

    Get ip from x-forwarded-for.

--- src/middlewares/ip.parameter.decorator.ts
+++ src/middlewares/ip.parameter.decorator.ts
@@ -1,9 +1,17 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common'
 
+import { Request } from 'express'
+
 export const IPv4 = createParamDecorator(
   (data: undefined, ctx: ExecutionContext): string => {
-    const request = ctx.switchToHttp().getRequest()
-    const ip = request.ip
+    const request = ctx.switchToHttp().getRequest<Request>()
+    const xForwardedFor = request.headers['x-forwarded-for'] as string
+    let ip = request.ip
+
+    if (xForwardedFor) {
+      const forwardedIps = xForwardedFor.split(',')
+      ip = forwardedIps[0].trim()
+    }
 
     // Check for ::ffff: prefix and remove it if present
     if (ip.startsWith('::ffff:')) {
